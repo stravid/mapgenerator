32be21680e543971b2c1793daa973c296f61c369
Eberhard Graether <egraether.mmt-b2009@fh-salzburg.ac.at>
2010-04-09 02:44:06 +0200

    country.triangles

--- draw.js
+++ draw.js
@@ -5,6 +5,28 @@ function initMap()
     map = Raphael("map", 1000, 800);
 }
 
+function drawCountry(country)
+{
+    map.clear();
+    
+    var trianglesLength = country.triangles.length;
+    
+    for (var i = 0; i < trianglesLength; i++) {
+        for (var j = 0; j < 3; j++) {
+            
+            if (j == 2)
+                var k = 0;
+            else
+                var k = j+1;
+            
+            var line = "M " + country.triangles[i].vertex[j].x + " " + country.triangles[i].vertex[j].y;
+            line += "L " + country.triangles[i].vertex[k].x + " " + country.triangles[i].vertex[k].y;
+            
+            line = map.path(line); 
+        }
+    }
+}
+/*
 function drawCountry(triangles)  //(country)
 {
     map.clear();
@@ -24,4 +46,4 @@ function drawCountry(triangles)  //(country)
         }
     }
 }
-
+*/
